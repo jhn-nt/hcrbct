63f6f05ef91b0e5491a6b8def290008f0e964256
HashemFarhan <1hashemfarhan@gmail.com>
2024-09-29 19:35:15 -0400

    removed final_weight

--- procedures/demographics.sql
+++ procedures/demographics.sql
@@ -4,7 +4,6 @@ SELECT
     gender,
     ethnicity,
     admissionweight AS initial_weight,
-    dischargeweight AS final_weight,
 FROM physionet-data.eicu_crd.patient
 ORDER BY
     patientunitstayid ASC;
