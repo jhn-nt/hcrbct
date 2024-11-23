6c4137c30531173c0609ea5811d4b0120dafe1f1
Hashem Farhan <72375162+HashemFarhan@users.noreply.github.com>
2024-11-23 13:05:52 -0500

    Delete procedures/new_transfusion.sql
    Deleted extra query

--- procedures/new_transfusion.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    intake.patientunitstayid,
-    intake.intakeoutputid AS itemid,
-    intake.intakeoutputoffset AS offset,
-    intake.celllabel AS item,
-    intake.cellvaluenumeric AS value,
-    REGEXP_EXTRACT(intake.cellpath,r'(?:[a-zA-Z\s]*)\s\((?:[a-zA-Z]*)\)\|(?:[a-zA-Z\s]*)\s\(([a-zA-Z]*)\)\|(?:[a-zA-Z\s]*)') AS unit
-FROM physionet-data.eicu_crd.intakeoutput as intake
-WHERE intake.celllabel IN ('Volume-Transfuse red blood cells', 'PRBC', 'pRBCs')
